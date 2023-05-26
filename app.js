045b749fbea61f65af691f3a62b3f6dc6b7265dc
Rida A <56137490+ridaak@users.noreply.github.com>
2023-05-26 12:43:11 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const app = express();
+
+const port = 4000;
+
+app.get("/", (req, res) => {
+  res.send("This is an express application.");
+});
+
+app.listen(port, () => {
+  console.log("listening...");
+});
