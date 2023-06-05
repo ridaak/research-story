79899d0414b3da70e732fa8a8027dc2eedd9ee4a
ridaak <ridaak@github.com>
2023-06-05 15:12:33 +0200

    now returns index.html file

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
 
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.send("This is an express application!!!!!.");
+  res.sendFile(path.join(__dirname + "/index.html"));
 });
 
 app.listen(port, () => {
