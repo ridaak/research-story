57d49c6a25ba170b57e19c204e216c09a715e557
ridaak <ridaak@github.com>
2023-06-05 15:00:53 +0200

    added dockerfile

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const app = express();
 
-const port = process.env.port || 4000;
+const port = 3000;
 
 app.get("/", (req, res) => {
   res.send("This is an express application!!!!!.");
